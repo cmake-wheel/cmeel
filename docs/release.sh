bc5442821a099603d2e0b2520215fcbba81bd3e0
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-04-17 10:35:51 +0200

    setup release process

---
+++ docs/release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eux
+# ./docs/release.sh [patch|minor|major|x.y.z]
+
+[[ $(basename "$PWD") == docs ]] && cd ..
+
+
+OLD=$(poetry version -s)
+
+poetry version "$1"
+
+NEW=$(poetry version -s)
+DATE=$(date +%Y-%m-%d)
+
+sed -ri "/__version__ /s/[0-9.]+/$NEW/" cmw.py
+sed -i "/^## \[Unreleased\]/a \\\n## [v$NEW] - $DATE" CHANGELOG.md
+sed -i "/^\[Unreleased\]/s/$OLD/$NEW/" CHANGELOG.md
+sed -i "/^\[Unreleased\]/a [v$NEW]: https://github.com/nim65s/cmw/compare/v$OLD...v$NEW" CHANGELOG.md
+
+git add cmw.py pyproject.toml CHANGELOG.md
+git commit -m "Release v$NEW"
+git tag -s "v$NEW" -m "Release v$NEW"
+git push
+git push --tags
