c4141ea31ab98282a2ecbe508328d5effc64dcc0
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-04-20 00:20:34 +0200

    Release v0.4.0

--- cmeel.py
+++ cmeel.py
@@ -11,7 +11,7 @@ import os
 from packaging.tags import sys_tags
 import tomli
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 TEMP = Path(TemporaryDirectory(prefix="cmeel-").name)
 BUILD = TEMP / "bld"
