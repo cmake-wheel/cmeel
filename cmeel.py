39c098038e12b71bcaabb3d6124ffab5008b2717
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-04-20 00:20:13 +0200

    switch to pep 621

--- cmeel.py
+++ cmeel.py
@@ -1,62 +1,64 @@
 #!/usr/bin/env python
+
 from pathlib import Path
-from shutil import rmtree
+from shutil import move
 from subprocess import check_call, check_output
 from tempfile import TemporaryDirectory
 import logging
+import sys
+import os
 
+from packaging.tags import sys_tags
 import tomli
 
 __version__ = "0.3.0"
 
-DIR = TemporaryDirectory()
-BLD = DIR / "bld"
-PFX = DIR / "pfx"
-TAG = "from packaging.tags import sys_tags; print(next(sys_tags()))"
-PYTHON = "python"
+TEMP = Path(TemporaryDirectory(prefix="cmeel-").name)
+BUILD = TEMP / "bld"
+PREFIX = TEMP / "pfx"
+
+TAG = str(next(sys_tags()))
+SITE = os.sep.join(
+    ["lib", "python" + ".".join(sys.version.split(".")[:2]), "site-packages"]
+)
 
 DEPS = []  # TODO
-
-
-def get_requires_for_build_wheel(config_settings=None):
-    return ["packaging"]
 
 
 def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
     logging.info("CMake Wheel")
     logging.info("load conf from pyproject.toml")
     with open("pyproject.toml", "rb") as f:
-        conf = tomli.load(f)["tool"]["poetry"]
-    DIR.mkdir()
+        conf = tomli.load(f)["project"]
 
     logging.info("build wheel")
     # Configure
     distribution = conf["name"].replace("-", "_")
     logging.info("configure")
-    check_call(
-        f"cmake -S . -B {BLD}".split()
-        + [
-            "-DCMAKE_INSTALL_PREFIX="
-            f"{PFX}/{distribution}-{conf['version']}.data/data",
-            f"-DPYTHON_EXECUTABLE={PYTHON}",
-            "-DCMAKE_CXX_COMPILER_LAUNCHER=sccache",
-        ]
-    )
+    install = PREFIX / f"{distribution}-{conf['version']}.data" / "data"
+    check_call(["cmake", "-S", ".", "-B", BUILD, f"-DCMAKE_INSTALL_PREFIX={install}"])
+
     logging.info("build")
-    check_call(f"cmake --build {BLD}".split())
+    check_call(["cmake", "--build", BUILD])
     logging.info("test")
-    check_call(f"cmake --build {BLD} -t test".split())
+    check_call(["cmake", "--build", BUILD, "-t", "test"])
     logging.info("install")
-    check_call(f"cmake --install {BLD}".split())
+    check_call(["cmake", "--build", BUILD, "-t", "install"])
 
     logging.info("create dist-info")
-    dist_info = PFX / f"{distribution}-{conf['version']}.dist-info"
+    dist_info = PREFIX / f"{distribution}-{conf['version']}.dist-info"
     dist_info.mkdir()
     logging.info("create dist-info / METADATA")
     with open(conf["readme"]) as f:
         readme = f.read()
     requires = "\n".join([f"Requires-Dist: {dep}" for dep in DEPS])
     with (dist_info / "METADATA").open("w") as f:
+        if conf["readme"].lower().endswith(".md"):
+            content_type = "text/markdown"
+        elif conf["readme"].lower().endswith(".rst"):
+            content_type = "text/x-rst"
+        else:
+            content_type = "text/plain"
         f.write(
             "\n".join(
                 [
@@ -64,13 +66,12 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
                     f"Name: {distribution}",
                     f"Version: {conf['version']}",
                     f"Summary: {conf['description']}",
-                    f"Home-page: {conf['homepage']}",
-                    "Classifier: Programming Language :: Python :: 2",
+                    f"Home-page: {conf['urls']['homepage']}",
                     "Classifier: Programming Language :: Python :: 3",
                     "Classifier: License :: OSI Approved :: BSD License",
                     "Classifier: Operating System :: POSIX :: Linux",
-                    "Requires-Python: >= 2.7",
-                    "Description-Content-Type: text/markdown",
+                    f"Requires-Python: {conf['requires-python']}",
+                    f"Description-Content-Type: {content_type}",
                     f"{requires}",
                     "",
                     f"{readme}",
@@ -81,7 +82,6 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
     with (dist_info / "top_level.txt").open("w") as f:
         f.write("")
     logging.info("create dist-info / WHEEL")
-    tag = check_output(["python", "-c", TAG]).decode().strip()
     with (dist_info / "WHEEL").open("w") as f:
         f.write(
             "\n".join(
@@ -89,17 +89,17 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
                     "Wheel-Version: 1.0",
                     f"Generator: cmeel {__version__}",
                     "Root-Is-Purelib: false",
-                    f"Tag: {tag}",
+                    f"Tag: {TAG}",
                 ]
             )
         )
 
+    logging.info("move module")
+    for module in (install / SITE).glob("*"):
+        move(module, PREFIX)
+
     logging.info("wheel pack")
-    name = check_output(f"wheel pack -d {DIR} {PFX}".split()).decode()
+    name = check_output(["wheel", "pack", "-d", wheel_directory, PREFIX]).decode()
     name = name.split("/")[-1][:-6]
-    logging.info("move to {wheel_directory}")
-    (DIR / name).rename(Path(wheel_directory) / name)
-    logging.info("clean")
-    rmtree(DIR)
     logging.info("done")
     return name
