bf9d489280ce1662b8e735a31d4ed128e38d1303
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-04-26 07:39:47 +0200

    move helpers in dedicated file

--- cmeel/__main__.py
+++ cmeel/__main__.py
@@ -1,17 +1,11 @@
-"""Tools to help environment management."""
+"""Run cmeel as a python module."""
 
 import argparse
 import os
 import sys
 from pathlib import Path
 
-from .consts import CMEEL_PREFIX
-
-PATHS = {
-    "cmake": "CMAKE_PREFIX_PATH",
-    "lib": "LD_LIBRARY_PATH",
-    "pc": "PKG_CONFIG_PATH",
-}
+from .helpers import add_paths_arguments, get_paths
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -31,30 +25,9 @@ def get_parser() -> argparse.ArgumentParser:
         help="sub-command help",
     )
 
-    for cmd, path in PATHS.items():
-        sub = subparsers.add_parser(cmd, help=f"show cmeel additions to {path}")
-        sub.add_argument("--prepend", action="store_true", help=f"show full {path}")
-        sub.set_defaults(cmd=cmd)
+    add_paths_arguments(subparsers)
 
     return parser
-
-
-def get_paths(cmd: str, prepend: bool = False) -> str:
-    """Get the paths needed by the user."""
-    prefixes = [Path(path) / CMEEL_PREFIX for path in sys.path]
-    if cmd == "lib":
-        prefixes = [p / "lib" for p in prefixes]
-    elif cmd == "pc":
-        prefixes = [p / sub / "pkgconfig" for p in prefixes for sub in ["lib", "share"]]
-
-    available = [str(p) for p in prefixes if p.exists()]
-    if prepend:
-        ret = []
-        for prefix in available + os.environ.get(PATHS[cmd], "").split(os.pathsep):
-            if prefix and prefix not in ret:
-                ret.append(prefix)
-        return os.pathsep.join(ret)
-    return os.pathsep.join(available)
 
 
 if __name__ == "__main__":
---
+++ cmeel/helpers.py
@@ -0,0 +1,39 @@
+"""Tools to help environment management."""
+
+import os
+import pathlib
+import sys
+
+from .consts import CMEEL_PREFIX
+
+PATHS = {
+    "cmake": "CMAKE_PREFIX_PATH",
+    "lib": "LD_LIBRARY_PATH",
+    "pc": "PKG_CONFIG_PATH",
+}
+
+
+def get_paths(cmd: str, prepend: bool = False) -> str:
+    """Get the paths needed by the user."""
+    prefixes = [pathlib.Path(path) / CMEEL_PREFIX for path in sys.path]
+    if cmd == "lib":
+        prefixes = [p / "lib" for p in prefixes]
+    elif cmd == "pc":
+        prefixes = [p / sub / "pkgconfig" for p in prefixes for sub in ["lib", "share"]]
+
+    available = [str(p) for p in prefixes if p.exists()]
+    if prepend:
+        ret = []
+        for prefix in available + os.environ.get(PATHS[cmd], "").split(os.pathsep):
+            if prefix and prefix not in ret:
+                ret.append(prefix)
+        return os.pathsep.join(ret)
+    return os.pathsep.join(available)
+
+
+def add_paths_arguments(subparsers):
+    """Append paths commands for argparse."""
+    for cmd, path in PATHS.items():
+        sub = subparsers.add_parser(cmd, help=f"show cmeel additions to {path}")
+        sub.add_argument("--prepend", action="store_true", help=f"show full {path}")
+        sub.set_defaults(cmd=cmd)
