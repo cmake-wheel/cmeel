97b8029294a15d02f6c9bd2c997b9e6e4fd9a4cf
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-08-02 09:17:57 +0200

    noqa

--- cmeel/consts.py
+++ cmeel/consts.py
@@ -3,6 +3,6 @@ import os
 import sys
 
 CMEEL_PREFIX = "cmeel.prefix"
-SITELIB = os.sep.join(
+SITELIB = os.sep.join(  # noqa: PTH118
     ["lib", "python" + ".".join(sys.version.split(".")[:2]), "site-packages"],
 )
