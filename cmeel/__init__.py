55afadecde5786918e19555ba5de667ea3f132f9
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-08-04 18:01:30 +0200

    expose entrypoints in main module

--- cmeel/__init__.py
+++ cmeel/__init__.py
@@ -2,6 +2,8 @@
 
 from importlib.metadata import metadata
 
+from .build import build_editable, build_sdist, build_wheel  # noqa: F401
+
 __metadata__ = metadata("cmeel")
 __project_name__ = __metadata__["name"]
 __version__ = __metadata__["version"]
