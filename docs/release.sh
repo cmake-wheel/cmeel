ce61e20fffdc3bbc41be25e3a4bb011fa45543f0
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-12-31 15:31:18 +0100

    hatch wont update pyproject.toml
    ```
    Cannot set version when it is statically defined by the `project.version` field
    ```

--- docs/release.sh
+++ docs/release.sh
@@ -5,12 +5,10 @@
 
 
 OLD=$(hatch version)
-
-hatch version "$1"
-
-NEW=$(hatch version)
+NEW=$1
 DATE=$(date +%Y-%m-%d)
 
+sed -i "/^version =/s/$OLD/$NEW/" pyproject.toml
 sed -i "/^## \[Unreleased\]/a \\\n## [v$NEW] - $DATE" CHANGELOG.md
 sed -i "/^\[Unreleased\]/s/$OLD/$NEW/" CHANGELOG.md
 sed -i "/^\[Unreleased\]/a [v$NEW]: https://github.com/cmake-wheel/cmeel/compare/v$OLD...v$NEW" CHANGELOG.md
